338be32d0413cb74f27d2a20e3cdc411290f9e10
Elio <elio.sanfratello@gmail.com>
2023-12-11 17:14:00 +0100

    ControlledForms4

--- ControlledForms/src/Login.jsx
+++ ControlledForms/src/Login.jsx
@@ -1,19 +1,22 @@
 import { useState } from "react";
 
-export function Login({onLogin}) {
+export function Login({ onLogin }) {
     const [data, setData] = useState({
         username: "",
-        password: ""
+        password: "",
+        session: false,
     })
-    
+
     function handleInputChange(event) {
         const name = event.target.name
         const value = event.target.value
-        
+        const checked = event.target.checked
+        const type = event.target.type
+
         setData(d => {
             return {
                 ...data,
-                [name]: value,
+                [name]: type === "checkbox" ? checked : value,
             }
         })
     }
@@ -21,14 +24,26 @@ export function Login({onLogin}) {
     function onLogin(event) {
         console.log(data)
     }
-    
+
+    function handleReset() {
+        setData({
+            username: '',
+            password: '',
+            session: false
+        })
+    }
+
     return (
         <div>
             <input name="username" value={data.username} onInput={handleInputChange} />
             <input name="password" type="password" value={data.password} onInput={handleInputChange} />
             <button name="login" disabled={!data.username || !data.password} onClick={onLogin}>Login</button>
+            <button name="reset" onClick={handleReset}>Reset</button>
             <label>Ricordami</label>
-            <input type="checkbox" id="checkbox" />
+            <input name="session" type="checkbox" checked={data.session} onChange={handleInputChange} />
+            <pre>
+                {JSON.stringify(data, null, 2)}
+            </pre>
         </div>
     )
 }
