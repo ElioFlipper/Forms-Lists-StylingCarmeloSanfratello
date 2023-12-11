6ec78ab2fd0a82f73bcc9a0251a0a5d25b06d8e0
Elio <elio.sanfratello@gmail.com>
2023-12-11 16:55:55 +0100

    Controlled forms3

--- ControlledForms/src/Login.jsx
+++ ControlledForms/src/Login.jsx
@@ -1,15 +1,15 @@
 import { useState } from "react";
 
-export function Login() {
+export function Login({onLogin}) {
     const [data, setData] = useState({
         username: "",
         password: ""
     })
-
+    
     function handleInputChange(event) {
         const name = event.target.name
         const value = event.target.value
-
+        
         setData(d => {
             return {
                 ...data,
@@ -18,14 +18,17 @@ export function Login() {
         })
     }
 
+    function onLogin(event) {
+        console.log(data)
+    }
+    
     return (
         <div>
             <input name="username" value={data.username} onInput={handleInputChange} />
             <input name="password" type="password" value={data.password} onInput={handleInputChange} />
-            <button disabled={!data.username || !data.password}>Login</button>
-            <label for="id">Ricordami</label>
+            <button name="login" disabled={!data.username || !data.password} onClick={onLogin}>Login</button>
+            <label>Ricordami</label>
             <input type="checkbox" id="checkbox" />
-
         </div>
     )
 }
