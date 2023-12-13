a989a702d9e731dbec815970fe040eb38685ace1
Elio <elio.sanfratello@gmail.com>
2023-12-13 09:34:07 +0100

    UncontrolledForms2

--- ControlledForms/src/UncontrolledLogin.jsx
+++ ControlledForms/src/UncontrolledLogin.jsx
@@ -1,27 +1,28 @@
-export function UncontrolledLogin(){
+export function UncontrolledLogin() {
 
     function handleForm(event) {
 
         event.preventDefault()
 
-        const username = event.target.elements.namedItem("username").value
-        const password = event.target.elements.namedItem("password").value
-        const session = event.target.elements.namedItem("session").checked
+        const formData = new FormData(event.target)
 
         const data = {
-            username,
-            password,
-            session
+            username: formData.get("username"),
+            password: formData.get("password"),
+            session: formData.get("session") === "on" ? true : false,
         }
         console.log(data)
     }
 
+    //I vantaggi sono che probabilmente il codice è più rapido e semplice ma si potrebbe incappare più facilmente in errori in caso di 
+    //valori più "particolari" (vedi session in cui deve usarsi un operatore ternario)
+
     return (
         <form onSubmit={handleForm}>
             <h1>Uncontrolled Login</h1>
-            <input name= "username" />
-            <input name= "password" type = "password" />
-            <input name= "session" type = "checkbox" />
+            <input name="username" />
+            <input name="password" type="password" />
+            <input name="session" type="checkbox" />
             <button>Login</button>
             <button type="reset">Reset</button>
         </form>
