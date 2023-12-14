aaa27986734d30e1c76978ec3ec0dd7247308aaa
Elio <elio.sanfratello@gmail.com>
2023-12-14 12:32:03 +0100

    lists State2

--- ControlledForms/src/App.jsx
+++ ControlledForms/src/App.jsx
@@ -7,15 +7,8 @@ import { ToDoList } from "./ListsAndState";
 export function App() {
   return (
     <div>
-      <Colors colors={[
-        {id:1, name:"yellow"},
-        {id:2, name:"red"},
-        {id:3, name:"green"},
-        {id:4, name:"blue"},
-        {id:5, name:"marrò"},
-      ]}/>
 
-      <ToDoList />
+      <ToDoList todos={["","","","","","",""]}/>
     </div>
   )
 } 
--- ControlledForms/src/ListsAndState.jsx
+++ ControlledForms/src/ListsAndState.jsx
@@ -1,29 +1,48 @@
 import { useState } from "react"
 
-export function ToDoList(){
+function ToDo() {
 
     const [todos, setTodos] = useState([])
     const [input, setInput] = useState("")
 
-    function handlePush(event){
+    function handlePush(event) {
+        event.preventDefault()
         setTodos([...todos, input])
-        setInput("")
     }
 
-    function handleInputChange(event){
+
+    function handleInputChange(event) {
+        event.preventDefault()
         setInput(event.target.value)
     }
 
+    function handleReset(event) {
+        setInput("")
+    }
 
-    return(
+    return (
+        <form>
         <div>
             <ul>
                 <li>
                     <h1>{todos}</h1>
                 </li>
             </ul>
-            <button onClick={handlePush}>Click me</button>
+            <button name = "OnClick" onClick={handlePush} >Click me</button>
+            <button name = "Reset" onClick={handleReset}>Reset</button>
             <input onChange={handleInputChange} />
+        </div>
+        </form>
+    )
+
+}
+
+export function ToDoList({ todos }) {
+    return (
+        <div>
+            {todos.map((item) => (
+                <ToDo key={item} item={item} />
+            ))}
         </div>
     )
 }
