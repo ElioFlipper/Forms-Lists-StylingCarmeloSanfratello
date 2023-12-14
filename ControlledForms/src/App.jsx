24d1886dd43de5831f6ae6c38043afe066ec5f03
Elio <elio.sanfratello@gmail.com>
2023-12-14 16:54:05 +0100

    list and state 3

--- ControlledForms/src/App.jsx
+++ ControlledForms/src/App.jsx
@@ -3,12 +3,15 @@ import { Colors } from "./Colors";
 import { Counter } from "./Counter";
 import { FocusableInput } from "./FocusableInput";
 import { ToDoList } from "./ListsAndState";
+import { ToDo } from "./ToDo";
+import { ToDo2 } from "./ToDoProva";
+
 
 export function App() {
   return (
     <div>
+      <ToDo2 />
 
-      <ToDoList todos={["","","","","","",""]}/>
     </div>
   )
 } 
